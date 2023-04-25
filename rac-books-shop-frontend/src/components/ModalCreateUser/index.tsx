f1109936c5e429253c86b4f306b90d31baa817c6
rafacancian <cancian.rafa@gmail.com>
2023-04-25 19:02:35 +0100

    creating action to open modal register user

---
+++ rac-books-shop-frontend/src/components/ModalCreateUser/index.tsx
@@ -0,0 +1,94 @@
+import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks"
+import { useState } from "react"
+
+import loginImg from './assets/login.png'
+import './ModalCreateUser.css'
+
+interface PropsModalCreateUser {
+    opened: boolean
+    whenClose: () => void
+}
+
+const ModalCreateUser = (props : PropsModalCreateUser) => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [address, setAddress] = useState('')
+    const [complement, setComplement] = useState('')
+    const [cep, setCep] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmed, setPasswordConfirmed] = useState('')
+
+    const aoSubmeterFormular = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        const user = {
+            name,
+            email,
+            password,
+            address,
+            cep,
+            complement
+        }
+        console.log("Action to register user: " +user)
+        alert('User create with success!')
+        // TODO create http.post to microservice users
+    }
+
+    return (
+        <AbModal 
+            titulo="Register" 
+            aberta={props.opened}
+            aoFechar={props.whenClose}
+        >
+        <section className="createUserBody">
+            <figure>
+                <img src={loginImg} alt="Register user icon" />
+            </figure>
+            <form onSubmit={aoSubmeterFormular}>
+                <AbCampoTexto 
+                    label="Name"
+                    value={name}
+                    onChange={setName}
+                />
+                <AbCampoTexto 
+                    label="Email"
+                    value={email}
+                    onChange={setEmail}
+                    type="email"
+                />
+                <AbCampoTexto 
+                    label="Address"
+                    value={address}
+                    onChange={setAddress}
+                />
+                <AbCampoTexto 
+                    label="Complement"
+                    value={complement}
+                    onChange={setComplement}
+                />
+                <AbCampoTexto 
+                    label="CEP"
+                    value={cep}
+                    onChange={setCep}
+                />
+                <AbCampoTexto 
+                    label="Password"
+                    value={password}
+                    onChange={setPassword}
+                    type="password"
+                />
+                <AbCampoTexto 
+                    label="Confirm password"
+                    value={passwordConfirmed}
+                    onChange={setPasswordConfirmed}
+                    type="password"
+                />
+                <div className="acoes">
+                    <AbBotao texto="Register"/>
+                </div>
+            </form>
+        </section>
+    </AbModal>)
+}
+
+export default ModalCreateUser
--- rac-books-shop-frontend/src/components/NavBar/index.tsx
+++ rac-books-shop-frontend/src/components/NavBar/index.tsx
@@ -3,48 +3,54 @@ import logo from './assets/logo.png'
 import './NavBar.css'
 import NavButton from "../NavButton"
 import userImg from './assets/usuario.svg'
+import ModalCreateUser from "../ModalCreateUser"
+import { useState } from "react"
 
 const NavBar = () => {
+
+    const [modalOpened, setModalOpened] = useState(false)
+
     return (
-    <nav className="ab-navbar">
-        <h1 className="logo">
-            <Link to="/">
-                <img className="logo" src={logo} alt="RAC Books logo" />
-            </Link>
-        </h1>
-        <ul className="navegacao">
-            <li>
-                <a href="#!">Categories</a>
-                <ul className="submenu">
-                    <li>
-                        <Link to="/">Backend</Link>
-                    </li>
-                    <li>
-                        <Link to="/">Frontend</Link>
-                    </li>
-                    <li>
-                        <Link to="/">Devops</Link>
-                    </li>
-                    <li>
-                        <Link to="/">Design e UX</Link>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <ul className="acoes">
-            <li>
-                <NavButton text="Login" textAltSrc="Icone representando um usuário" imageSrc={userImg} />
-            </li>
-            <li>
-                <NavButton
-                    text="Cadastrar-se"
-                    textAltSrc="Icone representando um usuário"
-                    imageSrc={userImg}
-                />
-                {/* <ModalCadastroUsuario /> */}
-            </li>
-        </ul>
-    </nav>)
+        <nav className="ab-navbar">
+            <h1 className="logo">
+                <Link to="/">
+                    <img className="logo" src={logo} alt="RAC Books logo" />
+                </Link>
+            </h1>
+            <ul className="navegacao">
+                <li>
+                    <a href="#!">Categories</a>
+                    <ul className="submenu">
+                        <li>
+                            <Link to="/">Backend</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Frontend</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Devops</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Design e UX</Link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <ul className="acoes">
+                <li>
+                    <NavButton text="Login" textAltSrc="User icon" imageSrc={userImg} />
+                </li>
+                <li>
+                    <NavButton text="Create Account" textAltSrc="User icon" imageSrc={userImg}
+                        onClick={() => setModalOpened(true)}/>
+                    <ModalCreateUser 
+                        opened={modalOpened}
+                        whenClose={() => setModalOpened(false)}
+                    />
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar
