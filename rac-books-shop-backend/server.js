f4a67f8203a6cbe50beb526367a263996d74dc66
rafacancian <cancian.rafa@gmail.com>
2023-05-04 22:14:32 +0100

    creating endpoint to register user

--- rac-books-shop-backend/server.js
+++ rac-books-shop-backend/server.js
@@ -14,7 +14,7 @@ server.use(jsonServer.defaults());
 const SECRET_KEY = 'RAC-BOOKS-KEY'
 
 function createToken(payload, expiresIn = '12h') {
-    return jwt.sign(payload, SECRET_KEY, {expiresIn})
+    return jwt.sign(payload, SECRET_KEY, { expiresIn })
 }
 
 function verifyToken(token) {
@@ -23,17 +23,43 @@ function verifyToken(token) {
     })
 }
 
-function verifyUserExist({email, password}) {
+function verifyUserExist({ email, password }) {
     return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
 }
 
 server.post('/public/register', (req, res) => {
-    const {email, password, name, address, complement, cep } = req.body;
+    const { email, password, name, address, complement, cep } = req.body;
 
-    if(verifyUserExist(email, password)) {
+    if (verifyUserExist(email, password)) {
         const status = 401;
         const message = "User already registered";
-        res.status(status).json({status, message});
+        res.status(status).json({ status, message });
         return
     }
+
+    fs.readFile("./users.json", (error, data) => {
+        if (error) {
+            const status = 401
+            const message = error
+            res.status(status).json({ status, message })
+            return
+        }
+
+        const json = JSON.parse(data.toString());
+        const lastItemId = json.users.length > 0 ? json.users[json.users.length - 1].id : 0;
+
+        json.users.push({ id: lastItemId + 1, email, password, name, address, complement, cep });
+        fs.writeFile("./users.json", JSON.stringify(json), (error) => {
+            if (error) {
+                const status = 401
+                const message = error
+                res.status(status).json({ status, message })
+                return
+            }
+        });
+        userdb = json;
+    });
+
+    const access_token = createToken({ email, password })
+    res.status(200).json({ access_token})
 })
