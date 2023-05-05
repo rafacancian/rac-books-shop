ca724adeea23b4719b77caa06ea71c75644011b6
rafacancian <cancian.rafa@gmail.com>
2023-05-05 19:43:49 +0100

    refactor imagens and logos

--- rac-books-shop-frontend/src/components/Footer/index.tsx
+++ rac-books-shop-frontend/src/components/Footer/index.tsx
@@ -2,80 +2,52 @@ import './Footer.css'
 
 const Footer = () => {
     return (<footer className="rodape">
-        <h2 className="rodape__titulo">Grupo Alura</h2>
+        <h2 className="rodape__titulo">RAC Group</h2>
         <ul className="lista-rodape">
-            <li className="lista-rodape__titulo">Educação</li>
+            <li className="lista-rodape__titulo">Company</li>
             <li className="lista-rodape__item">
-                <img src="/imagens/CasaDoCodigo.svg" alt="Logo da casa do código" />
-                <a href="#!" className="lista-rodape__link">Casa do código</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/Caelum.svg" alt="Logo da caelum" />
-                <a href="#!" className="lista-rodape__link">Caelum</a>
+                <img src="/imagens/company.png" alt="company logo" />
+                <a href="#!" className="lista-rodape__link">Info about company</a>
             </li>
         </ul>
 
         <ul className="lista-rodape">
-            <li className="lista-rodape__titulo">Educação online</li>
+            <li className="lista-rodape__titulo">Partners</li>
             <li className="lista-rodape__item">
-                <img src="/imagens/Alura.svg" alt="Logo da Alura" />
-                <a href="#!" className="lista-rodape__link">Alura</a>
+                <img src="/imagens/company.png" alt="Partner 01 logo" />
+                <a href="#!" className="lista-rodape__link">Partner 01</a>
             </li>
             <li className="lista-rodape__item">
-                <img src="/imagens/AluraEmpresas.svg" alt="Logo da Alura para Empresas" />
-                <a href="#!" className="lista-rodape__link">Alura para Empresas</a>
+                <img src="/imagens/company.png" alt="Partner 02 logo" />
+                <a href="#!" className="lista-rodape__link">Partner 02</a>
             </li>
             <li className="lista-rodape__item">
-                <img src="/imagens/AluraLATAM.svg" alt="Logo da Alura Latam" />
-                <a href="#!" className="lista-rodape__link">Alura LATAM</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/AluraStart.svg" alt="Logo da Alura START" />
-                <a href="#!" className="lista-rodape__link">Alura Start</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/MusicDot.svg" alt="Logo da Music Dot" />
-                <a href="#!" className="lista-rodape__link">Music Dot</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/AluraLingua.svg" alt="Logo da Alura Lingua" />
-                <a href="#!" className="lista-rodape__link">Alura Lingua</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/PM3.svg" alt="Logo da PM3" />
-                <a href="#!" className="lista-rodape__link">PM3</a>
+                <img src="/imagens/company.png" alt="Partner 03 logo" />
+                <a href="#!" className="lista-rodape__link">Partner 03</a>
             </li>
         </ul>
 
         <ul className="lista-rodape">
-            <li className="lista-rodape__titulo">Comunidade</li>
+            <li className="lista-rodape__titulo">Social Midias</li>
             <li className="lista-rodape__item">
-                <img src="/imagens/HipstersTech.svg" alt="Logo do Hipsters ponto Tech" />
-                <a href="#!" className="lista-rodape__link">Hipsters ponto Tech</a>
+                <img src="/imagens/contact/instagram.png" alt="Instagram logo" />
+                <a href="#!" className="lista-rodape__link">Instagram</a>
             </li>
             <li className="lista-rodape__item">
-                <img src="/imagens/ScubaDev.svg" alt="Logo do Scuba Dev" />
-                <a href="#!" className="lista-rodape__link">Scuba Dev</a>
+                <img src="/imagens/contact/facebook.png" alt="Facebook logo" />
+                <a href="#!" className="lista-rodape__link">Facebook</a>
             </li>
             <li className="lista-rodape__item">
-                <img src="/imagens/LayersTech.svg" alt="Logo do Layers ponto Tech" />
-                <a href="#!" className="lista-rodape__link">Layers ponto Tech</a>
+                <img src="/imagens/contact/twitter.png" alt="Twitter logo" />
+                <a href="#!" className="lista-rodape__link">Twitter</a>
             </li>
             <li className="lista-rodape__item">
-                <img src="/imagens/LikeABoss.svg" alt="Logo do Like a Boss" />
-                <a href="#!" className="lista-rodape__link">Like a Boss</a>
+                <img src="/imagens/contact/whatsapp.png" alt="Whatsapp logo" />
+                <a href="#!" className="lista-rodape__link">Whatsapp</a>
             </li>
             <li className="lista-rodape__item">
-                <img src="/imagens/CarreiraSemFronteira.svg" alt="Logo do Carreira sem fronteiras" />
-                <a href="#!" className="lista-rodape__link">Carreira sem fronteiras</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/HipstersJobs.svg" alt="Logo do Hipsters ponto jobs" />
-                <a href="#!" className="lista-rodape__link">Hipsters ponto jobs</a>
-            </li>
-            <li className="lista-rodape__item">
-                <img src="/imagens/GUJ.svg" alt="Logo do GUJ" />
-                <a href="#!" className="lista-rodape__link">GUJ</a>
+                <img src="/imagens/contact/linkedin.png" alt="Linkedin logo" />
+                <a href="#!" className="lista-rodape__link">Linkedin</a>
             </li>
         </ul>
     </footer>)
