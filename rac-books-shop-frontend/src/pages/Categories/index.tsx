8ed1c6a0a1cde4c95906d4f3debde3b77bfd75a7
rafacancian <cancian.rafa@gmail.com>
2023-05-14 12:16:04 +0100

    create categories pages

---
+++ rac-books-shop-frontend/src/pages/Categories/index.tsx
@@ -0,0 +1,44 @@
+
+import { useParams } from "react-router-dom"
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import CategoryTitle from "../../components/CategoryTitle";
+import CategoryBooks from "../../components/CategoryBooks";
+import axios from "axios";
+import { ICategory } from "../../interfaces/ICategory";
+import { useQuery } from "@tanstack/react-query";
+
+const Categories = () => {
+
+    const getCategoryBySlug = async (slug: string) => {
+        console.log("outro getCategoryBySlug")
+        let response = await axios.get<ICategory>('http://localhost:8000/public/categories', {
+          params: {
+            slug
+          }
+        })
+        debugger
+        console.log("outro getCategoryBySlug result: " +response.data)
+
+        const myObject: ICategory = response.data as ICategory;
+        return myObject;
+    }
+
+    const params = useParams()
+    const { data: category, isLoading } = useQuery(['getCategoryBySlug', params.slug], () => getCategoryBySlug(params.slug || ''))
+
+    if (isLoading) {
+        return (<>
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+            </Box>
+        </>)
+    }
+
+    return (<section>
+        <CategoryTitle text={category?.name ?? ''} />
+        <CategoryBooks category={category!} />
+    </section>)
+}
+
+export default Categories
