ca88c16645dad59ee17a58b758430d5f07e623ef
rafacancian <cancian.rafa@gmail.com>
2023-05-15 23:41:26 +0100

    change getCategoryBySlud to usequery

--- rac-books-shop-frontend/src/http/index.ts
+++ rac-books-shop-frontend/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ICategory } from "../interfaces/ICategory";
 
 const http = axios.create({
     baseURL: "http://localhost:8000",
@@ -9,3 +10,12 @@ const http = axios.create({
 })
 
 export default http
+
+export const getCategoryBySlud = async (slug: string) => {
+    const response = await http.get<ICategory>("public/categories", {
+        params: {
+            slug
+        }
+    })
+    return response.data
+}
--- rac-books-shop-frontend/src/pages/Categories/index.tsx
+++ rac-books-shop-frontend/src/pages/Categories/index.tsx
@@ -1,20 +1,14 @@
 
 import { useParams } from "react-router-dom"
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
 import CategoryTitle from "../../components/CategoryTitle";
 import CategoryBooks from "../../components/CategoryBooks";
-import axios from "axios";
-import { ICategory } from "../../interfaces/ICategory";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import http from "../../http";
+import Loader from "../../components/Loader";
+import { getCategoryBySlud } from "../../http";
 
 const Categories = () => {
 
-    const [category, setCategory] = useState<ICategory>();
-    const params = useParams()
-
+    /* Changed to UseQuery  
     useEffect(() => {
         http.get<ICategory>("public/categories", {
             params: {
@@ -23,32 +17,19 @@ const Categories = () => {
         }).then(response => {
             setCategory(response.data)
         })
-    }, [params.slug])
+    }, [params.slug])*/
 
-    /*const getCategoryBySlug = async (slug: string) => {
-        console.log("getCategoryBySlug")
-        let response = http.get<ICategory>("public/categories", {
-            params: {
-                slug
-            }
-        })
-
-        const myObject: ICategory = response.data as ICategory;
-        return myObject;
-    }
-
-
-    //const { data: category, isLoading } = useQuery(['getCategoryBySlug', params.slug], () => getCategoryBySlug(params.slug || ''))
+    const params = useParams()
+    const { data: category, isLoading } = useQuery(["categoryBySlug", params.slug], () => getCategoryBySlud(params.slug || ""))
 
     if (isLoading) {
         return (<>
-            <Box sx={{ display: 'flex' }}>
-                <CircularProgress />
-            </Box>
+            <Loader />
         </>)
-    }*/
+    }
 
     return (<section>
+
         <CategoryTitle title={category?.name ?? ''} />
         <CategoryBooks category={category!} />
     </section>)
