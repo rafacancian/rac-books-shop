f8fb4286505e9e21f806792393fd5e128dc43884
rafacancian <cancian.rafa@gmail.com>
2023-05-21 16:45:37 +0100

    add search book component to books page

--- rac-books-shop-frontend/src/components/CategoryBooks/index.tsx
+++ rac-books-shop-frontend/src/components/CategoryBooks/index.tsx
@@ -2,17 +2,18 @@ import './CategoryBooks.css'
 import CardBook from '../CardBook'
 import { ICategory } from '../../interfaces/ICategory'
 //import { useQuery } from '@tanstack/react-query'
-import { getBooksByCategory } from '../../http'
 import { gql, useQuery } from '@apollo/client'
 import { IBook } from '../../interfaces/IBook'
+import { useState } from 'react'
+import { TextField } from '@mui/material'
 
 interface CategoryBooksProps {
   category: ICategory
 }
 
 const GET_BOOKS_BY_CATEGORY = gql`
-    query getBooksByCategory($categoryId: Int) {
-      books(categoryId : $categoryId) {
+    query getBooksByCategory($categoryId: Int, $title: String) {
+      books(categoryId : $categoryId, title : $title) {
         id,
         slug,
         title,
@@ -29,15 +30,29 @@ const CategoryBooks = ({ category }: CategoryBooksProps) => {
 
   //const { data: books } = useQuery(["booksByCategory", category], () => getBooksByCategory(category.id))
 
+  const [search, setSearch] = useState("");
+
   const { data } = useQuery<{ books: IBook[] }>(GET_BOOKS_BY_CATEGORY, {
     variables: {
-      categoryId: category.id
+      categoryId: category.id,
+      title: search
     }
   })
 
   return (
-    <section className="books">
-      {data?.books.map(book => <CardBook book={book} key={book.id} />)}
+    <section >
+      <form className='search-form'>
+        <TextField className='search-textfield' 
+          id="outlined-search" label="Search book" type="search" size='small' 
+          value={search} 
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setSearch(event.target.value);
+          }}
+        />
+      </form>
+      <div className="books">
+        {data?.books.map(book => <CardBook book={book} key={book.id} />)}
+      </div>
     </section>
   )
 }
