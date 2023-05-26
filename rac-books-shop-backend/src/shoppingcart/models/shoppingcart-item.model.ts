4b3348b0e9c6ec3f5b5beaafbafff26c77054c03
rafacancian <cancian.rafa@gmail.com>
2023-05-26 19:51:48 +0100

    update shopping cart schema

--- rac-books-shop-backend/src/shoppingcart/models/shoppingcart-item.model.ts
+++ rac-books-shop-backend/src/shoppingcart/models/shoppingcart-item.model.ts
@@ -6,13 +6,13 @@ import { Option } from 'src/books/models/options.model';
 export class ShoppingCartItem {
 
   @Field((type) => Number)
-  quantity: number;
-
-  @Field((type) => Number)
   bookId: number;
 
   @Field((type) => Number)
   optionId: number;
+
+  @Field((type) => Number)
+  quantity: number;
 
   @Field((type) => Option)
   option?: Option;
